d543e41bee5e2ee2168532cea8ecd5629b7dcda7
Alex-J-Jackson <alexander-jackson@outlook.com>
2022-08-01 15:43:14 +0100

    add ticket 4 GET /api/reviews/:review_id

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const app = express();
-const { getCategories } = require("./controllers/controllers");
+const { getCategories, getReviewById } = require("./controllers/controllers");
+const {
+  handleServerErrors,
+  handlePsqlErrors,
+  handleCustomErrors,
+} = require("./errors/errors");
 
 app.get("/api/categories", getCategories);
+app.get("/api/reviews/:review_id", getReviewById);
 
 // ERROR HANDLING
 
 app.all("/*", (req, res) => {
   res.status(404).send({ msg: "Route not found" });
 });
+
+app.use(handleCustomErrors);
+
+app.use(handlePsqlErrors);
+
+app.use(handleServerErrors);
 
 module.exports = app;
--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,7 +1,22 @@
-const { selectCategories } = require("../models/models");
+const { selectCategories, selectReviewById } = require("../models/models");
 
 exports.getCategories = (req, res, next) => {
-  selectCategories().then((categories) => {
-    res.status(200).send({ categories });
-  });
+  selectCategories()
+    .then((categories) => {
+      res.status(200).send({ categories });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.getReviewById = (req, res, next) => {
+  const { review_id } = req.params;
+  selectReviewById(review_id)
+    .then((review) => {
+      res.status(200).send({ review });
+    })
+    .catch((err) => {
+      next(err);
+    });
 };

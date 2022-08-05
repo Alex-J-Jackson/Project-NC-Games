8401d181e8eaa211e48ce8b5660791659d9158d4
Alex-J-Jackson <alexander-jackson@outlook.com>
2022-08-05 12:06:18 +0100

    add some refactoring

---
+++ models/valid-sorts-valid-orders.js
@@ -0,0 +1,14 @@
+exports.validSorts = [
+  "review_id",
+  "title",
+  "category",
+  "designer",
+  "owner",
+  "review_body",
+  "review_img_url",
+  "created_at",
+  "votes",
+  "comment_count",
+];
+
+exports.validOrders = ["asc", "desc"];
